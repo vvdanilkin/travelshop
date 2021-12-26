6cfb315c28d875520e689a730e55137db4d36e0e
Vladimir <vvdanilkin@gmail.com>
2021-12-26 16:43:07 +0300

    JPG and order

--- src/database/strengths.jsx
+++ src/database/strengths.jsx
@@ -3,37 +3,37 @@ const strengths = [
         id: '1',
         name: 'reliable travel company',
         subtitle: 'Our company is included into the Common Federal Register of tour agencies.',
-        image: "../assets/icon1.jpg"
+        image: "../assets/icon1.JPG"
     },
     {
         id: '2',
         name: 'qualified & experienced guides',
         subtitle: 'Our experienced licenced guides will make your tour unforgettable.',
-        image: "../assets/icon2.jpg"
+        image: "../assets/icon2.JPG"
     },
     {
         id: '3',
         name: 'wide variety of tours',
         subtitle: 'We provide both standard tour services and tailor-made solutions.',
-        image: "../assets/icon3.jpg"
+        image: "../assets/icon3.JPG"
     },
     {
         id: '4',
         name: 'over 1000 satisfied clients',
         subtitle: 'We helped over one thousand guests to enjoy their stay in Moscow.',
-        image: "../assets/icon4.jpg"
+        image: "../assets/icon4.JPG"
     },
     {
         id: '5',
         name: '24-hour call service',
         subtitle: 'Our clients are provided with 24-hour call service for any issue.',
-        image: "../assets/icon5.jpg"
+        image: "../assets/icon5.JPG"
     },
     {
         id: '6',
         name: 'customer loyalty program',
         subtitle: 'We appreciate each of our client and offer a customer loyalty program.',
-        image: "../assets/icon6.jpg"
+        image: "../assets/icon6.JPG"
     }
 ];
 
